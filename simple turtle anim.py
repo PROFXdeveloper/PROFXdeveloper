bf2a147cc5d3b1314f0e44321809f612639821fd
John Jarvey <88535836+PROFXdeveloper@users.noreply.github.com>
2021-09-30 02:49:47 -0700

    Create simple turtle anim.py

---
+++ simple turtle anim.py
@@ -0,0 +1,14 @@
+import turtle
+
+# Loop for pattern
+for i in range(100):
+	
+# move the turtle forward by
+# 100+variable unit distance
+# in the direction of head of
+# turtle
+ turtle.forward(100+10*i)
+	
+
+# by 90 degrees to the left.
+ turtle.left(90)
